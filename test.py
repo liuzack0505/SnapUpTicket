c92f10849cae7262ec07348f10f146ae87452598
110062329 <liu.zack0505@gmail.com>
2024-02-12 20:20:28 +0800

    update v2.0

--- test.py
+++ test.py
@@ -24,24 +24,22 @@ try:
     email = driver.find_element(By.ID, 'user_login')
     email.clear()
     email.send_keys(GMAIL)
-    # time.sleep(HUMAN_DELAY) ## inmitate human
     password = driver.find_element(By.ID, 'user_password')
     password.clear()
     password.send_keys(PASSWORD)
-    # time.sleep(HUMAN_DELAY) ## inmitate human
     driver.find_element(By.XPATH, '//*[@id="new_user"]/input[3]').click()
 except:
     print("無登入按鈕")
 
-# current_time = time.localtime()
-# target_time = time.mktime(time.strptime(f'{current_time.tm_year}-{current_time.tm_mon}-{current_time.tm_mday} 20:00:01', '%Y-%m-%d %H:%M:%S'))
-# wait_time = target_time - time.mktime(current_time)
+current_time = time.localtime()
+target_time = time.mktime(time.strptime(f'{current_time.tm_year}-{current_time.tm_mon}-{current_time.tm_mday} 20:00:01', '%Y-%m-%d %H:%M:%S'))
+wait_time = target_time - time.mktime(current_time)
 
-# if wait_time < 0:
-#     wait_time += 86400
+if wait_time < 0:
+    wait_time += 86400
 
-# print(f"{wait_time} 秒後，開始搶票")
-# time.sleep(wait_time)
+print(f"{wait_time} 秒後，開始搶票")
+time.sleep(wait_time)
 
 try:
     ## find plus button
@@ -50,7 +48,6 @@ try:
     length = len(buttons)
     if length >= 6:
         buttons[4].click()
-        # time.sleep(HUMAN_DELAY) ## inmitate human
         buttons[5].click()
     elif length >= 2:
         buttons[0].click()
@@ -68,14 +65,12 @@ try:
                 verification_code += i
         verification_code = unicodedata.normalize('NFKC', verification_code)
         codebox = driver.find_element(By.XPATH, '//*[@id="registrationsNewApp"]/div/div[5]/div[2]/div/div/div/div/div/div/div/input')
-        # time.sleep(HUMAN_DELAY) ## inmitate human
         codebox.clear()
         codebox.send_keys(verification_code)
 
     ## check box
     checkbox = driver.find_element(By.ID, 'person_agree_terms')
     if not checkbox.is_selected():
-        # time.sleep(HUMAN_DELAY) ## inmitate human
         checkbox.click()
     ## next step
     next_step = driver.find_element(By.XPATH, '//*[@id="registrationsNewApp"]/div/div[5]/div[4]/button')
