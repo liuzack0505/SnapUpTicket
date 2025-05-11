2c51844f2063108196c00c6b2b9829d5bdd1011b
liuzack0505 <liu.zack0505@gmail.com>
2025-05-11 15:36:46 +0800

    fix: fix the bug can't login

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import unicodedata
 import time
 import re
+import undetected_chromedriver as uc
 
 gmails = ["0982306031", "0928958175", "liu.zack0505@gmail.com"]
 passwords = ["0982306031", "0928958175", "83298329zack"]
@@ -33,9 +35,9 @@ while True:
 
 while True:
     try:
-        user_input = input("請輸入第二個位置號碼(按enter可以跳過):")  
+        user_input = input("請輸入第二個位置號碼(按enter可以跳過):")
         if user_input == "":
-            break;
+            break
         if int(user_input) > 0:
             seat2 = int(user_input)
             break
@@ -44,24 +46,28 @@ while True:
     except:
         print("error")
 
-driver = webdriver.Chrome()
+driver = uc.Chrome()
 driver.get(url)
-##sign in
+# sign in
 try:
-    isSignIn = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[4]/div/div/div[1]/div/div[2]/div/div[3]/a[2]')))
+    isSignIn = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+        (By.XPATH, '/html/body/div[3]/div[4]/div/div/div[1]/div/div[2]/div/div[3]/a[2]')))
     isSignIn.click()
+    time.sleep(5)
     email = driver.find_element(By.ID, 'user_login')
     email.clear()
     email.send_keys(GMAIL)
     password = driver.find_element(By.ID, 'user_password')
     password.clear()
     password.send_keys(PASSWORD)
-    driver.find_element(By.XPATH, '//*[@id="new_user"]/input[3]').click()
+    password.send_keys(Keys.RETURN)
+    time.sleep(5)
 except:
     print("無登入按鈕")
 
 current_time = time.localtime()
-target_time = time.mktime(time.strptime(f'{current_time.tm_year}-{current_time.tm_mon}-{current_time.tm_mday} 20:00:01', '%Y-%m-%d %H:%M:%S'))
+target_time = time.mktime(time.strptime(
+    f'{current_time.tm_year}-{current_time.tm_mon}-{current_time.tm_mday} 20:00:00', '%Y-%m-%d %H:%M:%S'))
 wait_time = target_time - time.mktime(current_time)
 
 if wait_time < 0:
@@ -71,57 +77,36 @@ print(f"{wait_time} 秒後，開始搶票")
 time.sleep(wait_time)
 
 try:
-    ## find plus button
+    # find plus button
     driver.refresh()
-    buttons = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'button.btn-default.plus')))
+    buttons = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, 'button.btn-default.plus')))
     length = len(buttons)
-    if seat1 is not None :
+    if seat1 is not None:
         if seat1 <= length:
             buttons[seat1 - 1].click()
         else:
             buttons[0].click()
+    time.sleep(0.5)
     if seat2 is not None:
         if seat2 <= length:
             buttons[seat2 - 1].click()
         else:
             buttons[1].click()
-    
-    ## find verification code
-    code_element = driver.find_elements(By.XPATH, '//*[@id="registrationsNewApp"]/div/div[5]/div[2]/div/div/div/div/div/p')
-    if code_element:
-        code_element = code_element[0].text
-        print("現在的問題是：", code_element)
-        match = re.search(r'\((.*?)\)', code_element)
-        chinese_numerals = match.group()[1:-1] if match else ""
-
-        chinese_to_arabic = {
-            '零': '0',
-            '一': '1',
-            '二': '2',
-            '三': '3',
-            '四': '4',
-            '五': '5',
-            '六': '6',
-            '七': '7',
-            '八': '8',
-            '九': '9'
-        }
-
-        verification_code = ''.join(chinese_to_arabic[ch] for ch in chinese_numerals)
-        
-        codebox = driver.find_element(By.XPATH, '//*[@id="registrationsNewApp"]/div/div[5]/div[2]/div/div/div/div/div/div/div/input')
-        codebox.clear()
-        codebox.send_keys(verification_code)
-
-    ## check box
+    time.sleep(0.5)
+    # check box
     checkbox = driver.find_element(By.ID, 'person_agree_terms')
     if not checkbox.is_selected():
         checkbox.click()
-    ## next step
-    next_step = driver.find_element(By.XPATH, '//*[@id="registrationsNewApp"]/div/div[5]/div[4]/button')
+    time.sleep(0.5)
+    # next step
+    next_step = driver.find_element(
+        By.XPATH, '//*[@id="registrationsNewApp"]/div/div[5]/div[4]/button')
     next_step.click()
-    ## enter the check page(can comment this part when debugging)
-    end = WebDriverWait(driver, 600).until(EC.presence_of_element_located((By.XPATH, '//*[@id="registrations_controller"]/div[4]/div[2]/div/div[4]/a')))
+    # enter the check page(can comment this part when debugging)
+    time.sleep(0.5)
+    end = WebDriverWait(driver, 600).until(EC.presence_of_element_located(
+        (By.XPATH, '//*[@id="registrations_controller"]/div[4]/div[2]/div/div[4]/a')))
     end.click()
     print("搶票成功")
 except KeyError as e:
